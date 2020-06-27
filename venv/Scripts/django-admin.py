780d5c960f6b09aa5abf889703e5b0d70e735869
MokkoFm <mokko2070@mail.ru>
2020-06-28 00:42:39 +0300

    add post cards

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\mokko\desktop\liza-backend\venv\scripts\python.exe
+#!c:\users\mokko\desktop\blog-with-django\venv\scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
